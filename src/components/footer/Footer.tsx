87ae0703f9c41b9fe38233d2d2f5a502099d6dce
GusTiavo123 <luagus0@gmail.com>
2024-05-13 15:41:41 -0300

    Adding links to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ColumnProps } from "../../../types/Footer";
 import FooterColumn from "./FooterColumn";
 import SocialIcon from "./SocialIcon";
-
+import Link from "next/link";
 
 const Footer = () => {
   const columns: Array<ColumnProps> = [
@@ -41,25 +41,43 @@ const Footer = () => {
           ))}
           <div className="flex flex-col w-1/4">
             <div className="relative w-full h-full mt-auto">
-              <Image src="/logos/globit.png" alt="Globit logo" fill className="object-contain" />
+              <Image
+                src="/logos/globit.png"
+                alt="Globit logo"
+                fill
+                className="object-contain"
+              />
             </div>
             <div className="relative w-full h-full mt-auto">
-              <Image src="/logos/ASAEDE-1.png" alt="Asociación Argentina" fill className="object-contain" />
+              <Image
+                src="/logos/ASAEDE-1.png"
+                alt="Asociación Argentina"
+                fill
+                className="object-contain"
+              />
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-row max-w-6xl mx-auto my-3 justify-between items-center w-full">
         <div className="flex items-center space-x-3">
-          <SocialIcon src="/svg/linkedin.svg" alt="LinkedIn" />
-          <SocialIcon src="/svg/instagram.svg" alt="Instagram" />
+          <Link
+            href="https://www.linkedin.com/company/globit-me/"
+            target="_blank"
+          >
+            <SocialIcon src="/svg/linkedin.svg" alt="LinkedIn" />
+          </Link>
+          <Link href="https://www.instagram.com/globit.me" target="_blank">
+            <SocialIcon src="/svg/instagram.svg" alt="Instagram" />
+          </Link>
         </div>
         <div className="text-custom-black font-light opacity-60 text-sm">
-          © 2022 Globit Todos los derechos reservados, de Latinoamérica al mundo.
+          © 2024 Globit Todos los derechos reservados, de Latinoamérica al
+          mundo.
         </div>
       </div>
     </footer>
   );
 };
 
-export default Footer
+export default Footer;
