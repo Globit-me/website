30fad486815ad5406879f10a1e027e5942716cce
GusTiavo123 <luagus0@gmail.com>
2024-05-15 17:13:08 -0300

    Underline h3

--- src/components/footer/FooterColumn.tsx
+++ src/components/footer/FooterColumn.tsx
@@ -3,7 +3,7 @@ import { ColumnProps } from "../../../types/Footer";
 
 const FooterColumn: React.FC<ColumnProps> = ({ title, links }) => (
   <div className="flex flex-col w-full md:w-1/4 space-y-2 items-center md:items-start">
-    <h3 className="font-semibold mb-2 text-lg">{title}</h3>
+    <h3 className="font-semibold mb-2 text-lg underline">{title}</h3>
     {links.map((link) => (
       <Link href={link.href} key={link.href} className="text-sm md:text-base">
         {link.text}
