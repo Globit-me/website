5d07b2416322cb209eb447a7592591c1efa87162
Marc <marxdr7@gmail.com>
2024-05-21 13:38:52 -0300

    db setup

--- src/components/form/InputField.tsx
+++ src/components/form/InputField.tsx
@@ -17,7 +17,7 @@ const InputField: FC<InputFieldProps> = ({ type, id, label, register, error, dis
         id={id}
        {...register}
         disabled={disabled}
-        placeholder=" "
+        placeholder=""
         className={`peer block w-full border ${error ? 'border-red-500' : 'border-custom-blue'} rounded px-3 py-3 leading-tight focus:outline-none hover:ring-2 hover:ring-custom-blue focus:ring-2 focus:ring-custom-blue focus:border-custom-blue transition duration-300`}
       />
       <label
