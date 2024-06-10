c89586f39238e8fc6f5259a4cd5306e97a204d43
GusTiavo123 <luagus0@gmail.com>
2024-06-10 13:24:49 -0300

    chore: Update profile schema to use DateTime for date of birth and add validation for profile fields

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -31,9 +31,50 @@ export const RegisterSchema = z
     path: ["repassword"],
   });
 
-export const imageSchema = z.object({
-  file: z.instanceof(File).refine((file) => file.type.startsWith("image/"), {
-    message: "Only image files are allowed",
+const today = new Date();
+const eighteenYearsAgo = new Date(
+  today.getFullYear() - 18,
+  today.getMonth(),
+  today.getDate()
+);
+
+export const profileSchema = z.object({
+  dni: z
+    .string()
+    .min(1, {
+      message: "DNI inválido",
+    })
+    .regex(/^\d+$/, {
+      message: "El DNI debe contener solo números",
+    }),
+  birthday: z.date().refine((date) => date <= eighteenYearsAgo, {
+    message: "Debes ser mayor de 18 años",
+  }),
+  country: z.string().min(1, {
+    message: "País inválido",
+  }),
+  province: z.string().min(1, {
+    message: "Provincia inválida",
+  }),
+  address: z.string().min(1, {
+    message: "Dirección inválida",
+  }),
+  addressNumber: z
+    .string()
+    .min(1, {
+      message: "Número de dirección inválido",
+    })
+    .regex(/^\d+$/, {
+      message: "El número de dirección debe contener solo números",
+    }),
+  apartment: z.string().optional(),
+  dniFront: z
+    .instanceof(File)
+    .refine((file) => file.type.startsWith("image/"), {
+      message: "Solo se permiten imágenes",
+    }),
+  dniBack: z.instanceof(File).refine((file) => file.type.startsWith("image/"), {
+    message: "Solo se permiten imágenes",
   }),
 });
 
