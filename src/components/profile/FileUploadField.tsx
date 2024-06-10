a57313f02b98512a83cbe86fd98282d488e3f4c8
GusTiavo123 <luagus0@gmail.com>
2024-06-10 16:33:55 -0300

    chore: Update file upload and input fields in profile page

--- src/components/profile/FileUploadField.tsx
+++ src/components/profile/FileUploadField.tsx
@@ -4,18 +4,23 @@ interface FileUploadFieldProps {
   id: string;
   name: string;
   placeholder: string;
+  register: any;
+  error: string | undefined;
 }
 
 const FileUploadField: React.FC<FileUploadFieldProps> = ({
   id,
   name,
   placeholder,
+  register,
+  error,
 }) => (
   <div className="relative flex flex-col mb-4">
     <input
       type="file"
       id={id}
       name={name}
+      {...register}
       className="peer block w-full border rounded px-3 py-3 leading-tight border-custom-blue focus:outline-none hover:ring-2 hover:ring-custom-blue focus:ring-2 focus:ring-custom-blue focus:border-custom-blue transition duration-300"
       placeholder={placeholder}
     />
@@ -25,6 +30,7 @@ const FileUploadField: React.FC<FileUploadFieldProps> = ({
     >
       {placeholder}
     </label>
+    {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
   </div>
 );
 
