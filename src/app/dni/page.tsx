121031cfbd5f6c773eefb48fe71e888d28e667f3
GusTiavo123 <luagus0@gmail.com>
2024-06-12 18:23:40 -0300

    Update profile page with improved file upload and input fields

--- src/app/dni/page.tsx
+++ src/app/dni/page.tsx
@@ -61,7 +61,7 @@ const ProfilePage: React.FC = () => {
     <section className="max-w-6xl md:mx-auto mt-32 md:mt-56 mb-56 mx-6">
       <form onSubmit={handleSubmit(handleProfileUpdate)} className="w-full">
         <h2 className="text-2xl md:text-3xl font-bold text-custom-blue mb-6 md:mb-14 text-center">
-          Completar Perfil
+          Completar perfil
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
