dfab7f30c89cfc507c565a2ab1b50178dca24eaf
Marc <marxdr7@gmail.com>
2024-06-20 11:23:50 -0300

    fix: fixed test page that prevent correct build deploy

---
+++ prisma/migrations/20240620140922_init/migration.sql
@@ -0,0 +1,100 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "emailVerified" DATETIME,
+    "password" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "image" TEXT,
+    "dni" TEXT,
+    "dob" DATETIME,
+    "country" TEXT,
+    "province" TEXT,
+    "address" TEXT,
+    "addressNumber" TEXT,
+    "apartment" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "viewedDate" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+
+    PRIMARY KEY ("provider", "providerAccountId"),
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DNIImage" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "front" BLOB NOT NULL,
+    "reverse" BLOB,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "DNIImage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL DEFAULT 'Abierta',
+    "closedDate" DATETIME,
+    "amountSent" REAL NOT NULL,
+    "amountReceived" REAL NOT NULL,
+    "sendingBank" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_dni_key" ON "User"("dni");
+
+-- CreateIndex
+CREATE INDEX "User_dni_idx" ON "User"("dni");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_email_token_key" ON "VerificationToken"("email", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_email_token_key" ON "PasswordResetToken"("email", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DNIImage_userId_key" ON "DNIImage"("userId");
--- src/app/(protected)/test/page.tsx
+++ src/app/(protected)/test/page.tsx
@@ -1,80 +1,14 @@
-"use client";
+import { auth } from "@/auth";
 
-import { showDni, addDni } from "@/actions/profile";
-import { useState } from "react";
-import Modal from "react-modal";
-import Image from "next/image";
 
-export default function TestPage() {
-  const [file, setFile] = useState<File | null>(null);
-  const [dniImage, setDniImage] = useState<string | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleVerifyDni = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!file) {
-      console.error("No file selected");
-      return;
-    }
-    const data = new FormData();
-    data.append("file", file);
-    await addDni(data);
-  };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setFile(e.target.files[0]);
-    }
-  };
 
-  const handleShowDni = async () => {
-    const imageUrl = await showDni();
-    if (imageUrl) {
-      setDniImage(imageUrl);
-      setIsModalOpen(true);
-    }
-  };
-
-  const handleCloseModal = async () => {
-    setIsModalOpen(false);
-    setDniImage(null);
-  };
-
+export default async function TestPage() {
+  const session = await auth();
   return (
     <div className="flex flex-col max-w-6xl md:mx-auto mt-28 mb-8 md:mb-28 md:mt-56 mx-6">
       {JSON.stringify(session)}
-      <form action={handleSignOut}>
-        <button type="submit" >Cerrar Sesión</button>
-      </form>
-
-      <button
-        onClick={handleShowDni}
-        className="flex text-center p-5 mt-2 bg-custom-blue w-3/12 rounded-full text-white"
-      >
-        mostrar dni
-      </button>
-
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={handleCloseModal}
-        contentLabel="Large Image Modal"
-        className="flex justify-center items-center h-full"
-        overlayClassName="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center"
-      >
-        <div className="relative bg-white p-12 md:p-20 rounded shadow-lg max-w-full max-h-full">
-          <button
-            onClick={handleCloseModal}
-            className="absolute top-2 right-2 text-white bg-red-500 rounded-full p-2 focus:outline-none"
-          >
-            Cerrar
-          </button>
-          <div className="relative w-full h-auto">
-            {dniImage && (
-              <Image src={dniImage} alt="DNI" width={800} height={800} />
-            )}
-          </div>
-        </div>
-      </Modal>
     </div>
   );
 }
