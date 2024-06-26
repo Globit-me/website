815489c7fc6dc9aab1387135d3d73c518e4a3151
Marc <marxdr7@gmail.com>
2024-06-26 12:34:03 -0300

    chore: Update metadata description and openGraph in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,16 @@ import ContactSection from "@/components/contact/ContactSection";
 import Hero from "@/components/hero/Hero";
 import ImageCarousel from "@/components/ImageCarousel";
 import SectionFAQs from "@/components/SectionFAQs";
-import Head from "next/head";
+
 
 export default function Home() {
   return (
-    <>
-    <Head>
-      <link rel="canonical" href="https://globit.me" />
-    </Head>
-      <main className="max-w-6xl mx-auto ">
-      <Hero />
-      <ImageCarousel />
-      <BenefitsSection />
-      <SectionFAQs />
-      <ContactSection />
-    </main>
-    </>
-
+    <main className="max-w-6xl mx-auto ">
+    <Hero />
+    <ImageCarousel />
+    <BenefitsSection />
+    <SectionFAQs />
+    <ContactSection />
+  </main>
   );
 }
